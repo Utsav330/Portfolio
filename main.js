8ae4c38b7fb6776770717282e5da008441c0e553
Utsav330 <utsavtowards@gmail.com>
2024-07-26 06:51:37 -0700

    first commit

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var typed= new Typed(".text",{
-    strings:["Front-End Developer", "Back-End Developer", "Web-Designer"],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-   loop:true
+var typed = new Typed(".text", {
+    strings: ["Front-End Developer", "Back-End Developer", "Web-Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
 });
+
+let input1 = document.getElementById("input1");
+
+input1.addEventListener("input", function() {
+  let inputValue = input1.value;
+  input1.value = inputValue.trim().charAt(0).toUpperCase() + inputValue.trim().slice(1).toLowerCase();
+});
+
