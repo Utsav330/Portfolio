929d87033209afc2aeaf772d4206647e9179feff
Utsav330 <utsavtowards@gmail.com>
2024-08-01 20:06:56 +0545

    Changes on my website

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
-var typed = new Typed(".text", {
-    strings: ["Front-End Developer", "Back-End Developer", "Web-Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-});
+
 
 let input1 = document.getElementById("input1");
 
