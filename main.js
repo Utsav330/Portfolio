d725813df984ef38594ad18d259f2d30921d82d7
Utsav330 <utsavtowards@gmail.com>
2024-09-21 19:46:46 +0545

    My Updated Code

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 
 
-let input1 = document.getElementById("input1");
+function prevent(event) {
+  event.preventDefault();
+}
 
-input1.addEventListener("input", function() {
-  let inputValue = input1.value;
-  input1.value = inputValue.trim().charAt(0).toUpperCase() + inputValue.trim().slice(1).toLowerCase();
-});
 
